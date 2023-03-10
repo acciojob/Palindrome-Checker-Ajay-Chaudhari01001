627bf0729261b7527f3b27b45e58ce1c78d3c83c
Ajay Chaudhari <55138445+Ajay-Chaudhari01001@users.noreply.github.com>
2023-03-10 14:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 
 function palindrome(str){
 
+	// Convert string to lowercase and remove non-alphanumeric characters
+  str = str.toLowerCase().replace(/[\W_]/g, '');
+
+  // Reverse the string
+  var reversedStr = str.split('').reverse().join('');
+
+  // Compare the original string to the reversed string
+  return str === reversedStr;
+	}
 }
 module.exports = palindrome
